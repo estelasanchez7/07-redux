96558c8a8d736ff42718437382942aa2d0bd5791
estelasanchez7 <stelasv7@gmail.com>
2022-12-08 17:13:36 +0100

    Redux_Intro

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import TeoriaRedux from "./components/TeoriaRedux";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Redux</h1>
+      <hr />
+      <TeoriaRedux />
+    </div>
+  );
+}
+
+export default App;
