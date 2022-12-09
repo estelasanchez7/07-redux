35257050e001644e71ba109bf5e6c1a048afb963
estelasanchez7 <stelasv7@gmail.com>
2022-12-09 19:18:54 +0100

    CRUD_Edit

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,37 +1,65 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addTask } from '../features/tasks/taskSlice';
-import {v4 as uuid} from 'uuid'
-import {useNavigate} from 'react-router-dom'
-
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, editTask } from "../features/tasks/taskSlice";
+import { v4 as uuid } from "uuid";
+import { useNavigate, useParams } from "react-router-dom";
 
 const TaskForm = () => {
-    const [task, setTask] = useState({
-        title:'',
-        description:'',
-    })
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const params = useParams();
+  const tasks = useSelector((state) => state.tasks);
 
-    const handleChange=(e)=>{
-        setTask({
-            ...task,
-            [e.target.name]: e.target.value,
+  const handleChange = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (params.id) {
+      dispatch(editTask(task))
+    } else {
+      dispatch(
+        addTask({
+          ...task,
+          id: uuid(),
         })
+      );
     }
 
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        dispatch(addTask({
-            ...task,
-            id: uuid(),
-        }))
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (params.id) {
+      setTask(tasks.find((task) => task.id === params.id));
     }
+  }, []);
+
   return (
     <form onSubmit={handleSubmit}>
-      <input name='title' type="text" placeholder="title" onChange={handleChange}/>
-      <textarea name="description" placeholder="description" onChange={handleChange}></textarea>
+      <input
+        name="title"
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        value={task.title}
+      />
+      <textarea
+        name="description"
+        placeholder="description"
+        onChange={handleChange}
+        value={task.description}
+      ></textarea>
       <button>Save</button>
     </form>
   );
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,35 +1,32 @@
-import { useSelector, useDispatch } from "react-redux"
-import { deleteTask } from "../features/tasks/taskSlice"
-import {Link} from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux";
+import { deleteTask } from "../features/tasks/taskSlice";
+import { Link } from "react-router-dom";
 
 const TaskList = () => {
+  const tasks = useSelector((state) => state.tasks);
+  console.log(tasks);
+  const dispatch = useDispatch();
 
-    const tasks = useSelector (state => state.tasks)
-    console.log(tasks)
-    const dispatch = useDispatch()
-
-    const handleDelete = (id) =>{
-      dispatch(deleteTask(id))
-    }
-
+  const handleDelete = (id) => {
+    dispatch(deleteTask(id));
+  };
 
   return (
     <div>
       <header>
         <h1>Tasks {tasks.length}</h1>
-        <Link to='/create-task'>
-        Create Task
-        </Link>
+        <Link to="/create-task">Create Task</Link>
       </header>
-        {tasks.map(task=>(
-            <div key={task.id}>
-                <h3>{task.title}</h3>
-                <p>{task.description}</p>
-                <button onClick={()=> handleDelete(task.id)}>Delete</button>
-            </div>
-        ))}
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h3>{task.title}</h3>
+          <p>{task.description}</p>
+          <button onClick={() => handleDelete(task.id)}>Delete</button>
+          <Link to={`/edit-task/${task.id}`}>Edit</Link>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
