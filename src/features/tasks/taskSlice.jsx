da36b69adfc461136d79038ce7c4f601758dcb9b
estelasanchez7 <stelasv7@gmail.com>
2022-12-09 17:35:54 +0100

    CRUD_Delete

--- src/features/tasks/taskSlice.jsx
+++ src/features/tasks/taskSlice.jsx
@@ -1,30 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-    {
-        id: "1",
-        title: "Task 1",
-        completed: "false",
-        description: "This is a task",
-    },
-    {
-        id: "2",
-        title: "Task 2",
-        completed: "false",
-        description: "This is a task",
-    },
-] 
+  {
+    id: "1",
+    title: "Task 1",
+    completed: "false",
+    description: "This is a task",
+  },
+  {
+    id: "2",
+    title: "Task 2",
+    completed: "false",
+    description: "This is a task",
+  },
+];
 
 export const taskSlice = createSlice({
-    name:'tasks',
-    initialState,
-    reducers:{
-        addTask: (state,action) => {
-            state.push(action.payload);
-        }
+  name: "tasks",
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.push(action.payload);
+    },
+    deleteTask:(state,action)=>{
+      const taskFound = state.find(task => task.id === action.payload)
+      if(taskFound){
+        state.splice(state.indexOf(taskFound),1)
+      }
     }
-})
+  },
+});
 
-export const {addTask} = taskSlice.actions
+export const { addTask, deleteTask } = taskSlice.actions;
 
-export default taskSlice.reducer
+export default taskSlice.reducer;
