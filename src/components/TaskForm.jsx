f6867c3b2d1f5a1c4ed3873b977695c630b1fc40
estelasanchez7 <stelasv7@gmail.com>
2022-12-09 18:25:47 +0100

    CRUD_Update

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addTask } from '../features/tasks/taskSlice';
 import {v4 as uuid} from 'uuid'
+import {useNavigate} from 'react-router-dom'
 
 
 const TaskForm = () => {
@@ -11,6 +12,7 @@ const TaskForm = () => {
     })
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const handleChange=(e)=>{
         setTask({
